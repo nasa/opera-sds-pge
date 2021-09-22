366777b42819aa52f0b851a027b8fecd423755bc
Scott Collins <collinss@jpl.nasa.gov>
2021-09-22 10:00:12 -0700

    Initial addition of the src package
    Package defines a top-level opera namespace, and adds an initial partial adaptation of the util package from the NISAR PGE repository.

---
+++ src/opera/util/logger.py
@@ -0,0 +1,567 @@
+#
+# Copyright 2021, by the California Institute of Technology.
+# ALL RIGHTS RESERVED.
+# United States Government sponsorship acknowledged.
+# Any commercial use must be negotiated with the Office of Technology Transfer
+# at the California Institute of Technology.
+# This software may be subject to U.S. export control laws and regulations.
+# By accepting this document, the user agrees to comply with all applicable
+# U.S. export laws and regulations. User has the responsibility to obtain
+# export licenses, or other export authority as may be required, before
+# exporting such information to foreign countries or providing access to
+# foreign persons.
+#
+
+"""
+=========
+logger.py
+=========
+
+Logging utilities for use with OPERA PGEs.
+
+This module is adapted for OPERA from the NISAR PGE R2.0.0 util/logger.py
+Original Authors: Alice Stanboli, David White
+Adapted By: Scott Collins
+
+"""
+
+import csv
+import datetime
+import inspect
+import shutil
+import time
+
+from os.path import basename
+
+import opera.util.error_codes as error_codes
+import opera.util.time as time_util
+
+from .error_codes import ErrorCode
+from .usage_metrics import get_os_metrics
+
+
+def write(log_file, severity, workflow, module, error_code, error_location,
+          description):
+    """
+    Low-level logging function. May be called directly in lieu of PgeLogger class.
+
+    Parameters
+    ----------
+    log_file : file-like
+        The log file to write to.
+    severity : str
+        The severity level of the log message.
+    workflow : str
+        Name of the workflow where the logging took place.
+    module : str
+        Name of the module where the logging took place.
+    error_code : int or ErrorCode
+        The error code associated with the logged message.
+    error_location : str
+        File name and line number where the logging took place.
+    description : str
+        Description of the logged event.
+
+    """
+    time_tag = time_util.get_current_iso_time()
+
+    # TODO: rework into a string constant that this function calls .format on
+    message_str = time_tag + ", " + severity + ", " + workflow + ", " + module + ", " + \
+        str(error_code) + ", " + error_location + ", " + '"' + description + '"' + "\n"
+
+    log_file.write(message_str)
+
+
+def default_log_file_name():
+    """
+    Returns a path + filename that can be used for the log file right away.
+
+    To minimize the risk of errors opening a log file, the initial log filename
+    does not rely on anything read from a run config file, SAS output file, etc.
+    Therefore this filename does not follow the file naming convention.
+
+    Later (elsewhere), after everything is known, the log file will be renamed.
+
+    Returns
+    -------
+    file_path : str
+        Path to the default log file name.
+
+    """
+    log_datetime_str = time_util.get_time_for_filename(datetime.datetime.now())
+    file_path = "pge_{}.log".format(log_datetime_str)
+
+    return file_path
+
+
+def get_severity_from_error_code(error_code):
+    """
+    Determines the log level (Critical, Warning, Info, or Debug) based on the
+    provided error code.
+
+    Parameters
+    ----------
+    error_code : int or ErrorCode
+        The error code to map to a severity level.
+
+    Returns
+    -------
+    severity : str
+        The severity level associated to the provided error code.
+
+    """
+    # TODO: constants for max codes and severity strings
+    error_code_offset = error_code % 10000
+
+    if error_code_offset < error_codes.DEBUG_RANGE_START:
+        return "Info"
+
+    if error_code_offset < error_codes.WARNING_RANGE_START:
+        return "Debug"
+
+    if error_code_offset < error_codes.CRITICAL_RANGE_START:
+        return "Warning"
+
+    return "Critical"
+
+
+def standardize_severity_string(severity):
+    """
+    Returns the severity string in a consistent way.
+
+    Parameters
+    ----------
+    severity : str
+        The severity string to standardize.
+
+    Returns
+    -------
+    severity : str
+        The standardized severity string.
+
+    """
+    return severity.title()  # first char uppercase, rest lowercase.
+
+
+class PgeLogger:
+    """
+    Class to help with the PGE logging.
+
+    Advantages over the standalone write() function:
+    * Opens and closes the log file for you
+    * The class's write() function has fewer arguments that need to be provided.
+
+    """
+    LOGGER_CODE_BASE = 900000
+
+    def __init__(self, workflow=None, error_code_base=None,
+                 log_filename=None, existing_logfile_object=None, append=False):
+        """
+        Constructor opens the log file unless an existing_logfile_object is
+        specified
+
+        Parameters
+        ----------
+        workflow : str, optional
+            Name of the workflow this logger is associated to. Defaults
+            to "pge".
+        error_code_base : int, optional
+            The base error code value to associated to the logger. This gives
+            the logger a de-facto severity level. Defaults to the Info level
+            offset.
+        log_filename : str, optional
+            Path to write the log's contents to on disk. Ignored if
+            existing_logfile_object is provided. Defaults to the value
+            provided by default_log_file_name().
+        existing_logfile_object : file-like, optional
+            An existing open file-like object to write logged messages to.
+        append : bool, optional
+            If true, messages logged by this object are appended to the associated
+            log file. Otherwise, a new file is created.
+
+        """
+        self.start_time = time.monotonic()
+        self.log_count_by_severity = self._make_blank_log_count_by_severity_dict()
+
+        # TODO: consolidate existing_logfile_object and log_filename into a single arg
+        if existing_logfile_object:
+            self.log_file = existing_logfile_object
+        else:
+            if not log_filename:
+                log_filename = default_log_file_name()
+
+            if append:
+                self.log_file = open(log_filename, 'a')
+            else:
+                self.log_file = open(log_filename, 'w')
+
+        self._workflow = (workflow
+                          if workflow else f"pge_init::{basename(__file__)}")
+
+        self._error_code_base = (error_code_base
+                                 if error_code_base else PgeLogger.LOGGER_CODE_BASE)
+
+    def __del__(self):
+        """
+        Destructor for PgeLogger.
+
+        Ensures the internally managed log file is properly closed before this
+        object is garbage collected.
+
+        """
+        if self.log_file and not self.log_file.closed:
+            self.write_log_summary()
+            self.close_log_file()
+
+    def close_log_file(self):
+        if self.log_file and not self.log_file.closed:
+            self.info("PgeLogger", ErrorCode.CLOSING_LOG_FILE,
+                      f"Closing log file {self.get_file_name()}")
+            self.log_file.flush()
+            self.log_file.close()
+
+    @property
+    def workflow(self):
+        return self._workflow
+
+    @workflow.setter
+    def workflow(self, workflow: str):
+        self._workflow = workflow
+
+    @property
+    def error_code_base(self):
+        return self._error_code_base
+
+    @error_code_base.setter
+    def error_code_base(self, error_code_base: int):
+        self._error_code_base = error_code_base
+
+    def get_log_count_by_severity(self, severity):
+        """
+        Gets the number of messages logged for the specified severity
+
+        Parameters
+        ----------
+        severity : str
+            The severity level to get the log count of. Should be one of
+            info, debug, warning, critical (case-insensitive).
+
+        Returns
+        -------
+        log_count : int
+            The number of messages logged at the provided severity level.
+
+        """
+        try:
+            severity = standardize_severity_string(severity)
+            count = self.log_count_by_severity[severity]
+            return count
+        except KeyError:
+            return 0
+
+    @staticmethod
+    def _make_blank_log_count_by_severity_dict():
+        return {
+            "Debug": 0,
+            "Info": 0,
+            "Warning": 0,
+            "Critical": 0
+        }
+
+    def get_log_count_by_severity_dict(self):
+        """Returns a copy of the dictionary of log counts by severity."""
+        return self.log_count_by_severity.copy()
+
+    def increment_log_count_by_severity(self, severity):
+        """
+        Increments the logged message count of the provided severity level.
+
+        Parameters
+        ----------
+        severity : str
+            The severity level to increment the log count of. Should be one of
+            info, debug, warning, critical (case-insensitive).
+
+        """
+        try:
+            severity = standardize_severity_string(severity)
+            count = 1 + self.log_count_by_severity[severity]
+            self.log_count_by_severity[severity] = count
+        except KeyError:
+            # TODO: log this?
+            pass
+
+    def write(self, severity, module, error_code_offset, description,
+              additional_back_frames=0):
+        """
+        Write a message to the log.
+
+        Parameters
+        ----------
+        severity : str
+            The severity level to log at. Should be one of info, debug, warning,
+            critical (case-insensitive).
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+        additional_back_frames : int, optional
+            Number of call-stack frames to "back up" to in order to determine
+            the calling function and line number.
+
+        """
+        severity = standardize_severity_string(severity)
+        self.increment_log_count_by_severity(severity)
+
+        caller = inspect.currentframe().f_back
+
+        # TODO: Can the number of back frames be determined implicitly?
+        #       i.e. back up until the first non-logging frame is reached?
+        for _ in range(additional_back_frames):
+            caller = caller.f_back
+
+        location = caller.f_code.co_filename + ':' + str(caller.f_lineno)
+
+        write(self.log_file, severity, self.workflow, module,
+              self.error_code_base + error_code_offset,
+              location, description)
+
+    def info(self, module, error_code_offset, description):
+        """
+        Write an info-level message to the log.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+
+        """
+        self.write("Info", module, error_code_offset, description,
+                   additional_back_frames=1)
+
+    def debug(self, module, error_code_offset, description):
+        """
+        Write a debug-level message to the log.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+
+        """
+        self.write("Debug", module, error_code_offset, description,
+                   additional_back_frames=1)
+
+    def warning(self, module, error_code_offset, description):
+        """
+        Write a warning-level message to the log.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+
+        """
+        self.write("Warning", module, error_code_offset, description,
+                   additional_back_frames=1)
+
+    def critical(self, module, error_code_offset, description):
+        """
+        Write a critical-level message to the log.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+
+        """
+        self.write("Critical", module, error_code_offset, description,
+                   additional_back_frames=1)
+
+    def log(self, module, error_code_offset, description, additional_back_frames=0):
+        """
+        Logs any kind of message.
+
+        Determines the log level (Critical, Warning, Info, or Debug) based on
+        the provided error code offset.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        error_code_offset : int
+            Error code offset to add to the specified base to determine the
+            error code associated with the log message
+            TODO: should this be just the error code itself?
+        description : str
+            Description message to write to the log.
+        additional_back_frames : int, optional
+            Number of call-stack frames to "back up" to in order to determine
+            the calling function and line number.
+
+        """
+        severity = get_severity_from_error_code(error_code_offset)
+        self.write(severity, module, error_code_offset, description,
+                   additional_back_frames=additional_back_frames + 1)
+
+    def get_warning_count(self):
+        """Returns the number of messages logged at the warning level."""
+        return self.get_log_count_by_severity('Warning')
+
+    def get_critical_count(self):
+        """Returns the number of messages logged at the critical level."""
+        return self.get_log_count_by_severity('Critical')
+
+    def flush(self):
+        """Flushes the current contents of the log file to disk."""
+        self.log_file.flush()
+
+    def move(self, new_filename):
+        """
+        Closes the log file, moves/renames it, and re-opens it.
+
+        This function is useful when the log file has been given a default name,
+        and needs to be assigned a name that meets the PGE file naming conventions.
+
+        Parameters
+        ----------
+        new_filename : str
+            The new filename (including path) to assign to this log file.
+
+        """
+        old_filename = self.log_file.name
+        msg = "Moving log file from {} to {}".format(old_filename, new_filename)
+        print(msg)
+        self.info("PgeLogger", 900, msg)
+        self.log_file.close()
+        shutil.move(old_filename, new_filename)
+        self.log_file = open(new_filename, 'a')
+
+    def get_file_object(self):
+        """Return the file object for the current log file."""
+        return self.log_file
+
+    def get_file_name(self):
+        """Return the file name for the current log file."""
+        return self.log_file.name
+
+    def append_text_from_another_file(self, source_filename):
+        """
+        Appends text from another file to this log file.
+
+        Parameters
+        ----------
+        source_filename : str
+            File to read and append text from.
+
+        """
+        # TODO: check for file existence
+        with open(source_filename, 'r') as source_file_object:
+            source_contents = source_file_object.read()
+            self.log_file.write(source_contents)
+
+    def log_one_metric(self, module, metric_name, metric_value,
+                       additional_back_frames=0):
+        """
+        Writes one metric value to the log file.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module where the logging took place.
+        metric_name : str
+            Name of the metric being logged.
+        metric_value : object
+            Value to associate to the logged metric.
+        additional_back_frames : int
+            Number of call-stack frames to "back up" to in order to determine
+            the calling function and line number.
+
+        """
+        msg = "{}: {}".format(metric_name, metric_value)
+        self.log(module, ErrorCode.SUMMARY_STATS_MESSAGE, msg,
+                 additional_back_frames=additional_back_frames + 1)
+
+    def write_log_summary(self):
+        """
+        Writes a summary at the end of the log file, which includes totals
+        of each message logged for each severity level, OS-level metrics,
+        and total elapsed run time (since logger creation).
+
+        """
+        module_name = "PgeLogger"
+
+        # totals of messages logged
+        copy_of_log_count_by_severity = self.log_count_by_severity.copy()
+        for severity, count in copy_of_log_count_by_severity.items():
+            metric_name = "overall.log_messages." + severity.lower()
+            self.log_one_metric(module_name, metric_name, count)
+
+        # overall OS metrics
+        metrics = get_os_metrics()
+        for metric_name, value in metrics.items():
+            self.log_one_metric(module_name, "overall." + metric_name, value)
+
+        # Overall elapsed time
+        elapsed_time_seconds = time.monotonic() - self.start_time
+        self.log_one_metric(module_name, "overall.elapsed_seconds",
+                            elapsed_time_seconds)
+
+    def resync_log_count_by_severity(self):
+        """
+        Resynchronizes the dictionary of log counts by severity for all log
+        messages in the log file, including messages logged by anything external
+        to the PgeLogger class, such as an external SAS invoked by a PGE wrapper.
+
+        """
+        # TODO: is this method really necessary? if the log file was kept in
+        #       memory, could these tallies be kept in real-time?
+        if not self.log_file:
+            return
+
+        # flush any existing log messages to the file
+        self.flush()
+
+        # read the file and get a count of log messages for each severity
+        count_by_severity = self._make_blank_log_count_by_severity_dict()
+        log_filename = self.log_file.name
+
+        with open(log_filename, 'r', newline='') as in_file:
+            csv_reader = csv.reader(in_file)
+            for row in csv_reader:
+                if len(row) >= 2:
+                    try:
+                        severity = standardize_severity_string(row[1].strip())
+                        count_by_severity[severity] += 1
+                    except KeyError:
+                        pass
+
+        self.log_count_by_severity = count_by_severity
---
+++ src/opera/util/usage_metrics.py
@@ -0,0 +1,112 @@
+#
+# Copyright 2021, by the California Institute of Technology.
+# ALL RIGHTS RESERVED.
+# United States Government sponsorship acknowledged.
+# Any commercial use must be negotiated with the Office of Technology Transfer
+# at the California Institute of Technology.
+# This software may be subject to U.S. export control laws and regulations.
+# By accepting this document, the user agrees to comply with all applicable
+# U.S. export laws and regulations. User has the responsibility to obtain
+# export licenses, or other export authority as may be required, before
+# exporting such information to foreign countries or providing access to
+# foreign persons.
+#
+
+"""
+================
+usage_metrics.py
+================
+
+OS-level metrics gathering functions for use with OPERA PGEs.
+
+This module is adapted for OPERA from the NISAR PGE R2.0.0 util/usage_metrics.py
+Original Author: David White
+Adapted By: Scott Collins
+
+"""
+
+import os
+import resource
+
+
+def get_os_metrics():
+    """
+    Gets metrics related to machine resource usage, both by the current process
+    as well as all of its children processes.
+
+    Returns
+    -------
+    metrics : dict
+        Dictionary containing metrics mapped to the following keys
+            os.cpu.seconds.sys -
+                System CPU time, in seconds, consumed by the current process and
+                its children
+            os.cpu.seconds.user -
+                User CPU time, in seconds, consumed by the current process and
+                its children
+            os.filesystem.reads -
+                Number of file system reads performed by the current process and
+                its children
+            os.filesystem.writes -
+                Number of file system writes performed by the current process
+                and its children
+            os.max_rss_kb.main_process -
+                Maximum resident set size (physical memory consumption), in
+                kilobytes, of the current process
+            os.max_rss_kb.largest_child_process -
+                Maximum resident set size (physical memory consumption), in
+                kilobytes, of the largest child process
+            os.peak_vm_kb.main_process -
+                Peak virtual memory usage, in kilobytes, of the current process.
+
+    """
+    rusage_self = resource.getrusage(resource.RUSAGE_SELF)
+    rusage_children = resource.getrusage(resource.RUSAGE_CHILDREN)
+
+    metrics = {
+        # these metrics include the current process and "all children
+        # of the calling process that have terminated and been waited for";
+        # see the Linux man page for getrusage()
+        'os.cpu.seconds.sys': rusage_self.ru_stime + rusage_children.ru_stime,
+        'os.cpu.seconds.user': rusage_self.ru_utime + rusage_children.ru_utime,
+        'os.filesystem.reads': rusage_self.ru_inblock + rusage_children.ru_inblock,
+        'os.filesystem.writes': rusage_self.ru_oublock + rusage_children.ru_oublock,
+        # unfortunately the max_rss does not account for all child processes;
+        # see the Linux man page for getrusage()
+        'os.max_rss_kb.main_process': rusage_self.ru_maxrss,
+        'os.max_rss_kb.largest_child_process': rusage_children.ru_maxrss,
+        'os.peak_vm_kb.main_process': get_self_peak_vmm_kb()
+    }
+
+    return metrics
+
+
+def get_self_peak_vmm_kb():
+    """
+    Attempt to get the peak virtual memory by looking into the /proc/self/status
+
+    Note that this accounts for the peak virtual memory of just the current
+    process, not the sum of the current process and all its children.
+
+    Returns
+    -------
+    vm_peak_kb : int
+        Peak virtual memory usage, in kilobytes, of the current process. If
+        this value cannot be obtained for any reason, 0 is returned instead.
+
+    """
+    vm_peak_kb = 0
+
+    status_file = os.path.join(os.sep, 'proc', 'self', 'status')
+
+    if not os.path.exists(status_file) or not os.path.isfile(status_file):
+        return 'file_not_found: {}'.format(status_file)
+
+    with open(status_file, 'r') as infile:
+        for line in infile.readlines():
+            if line.startswith('VmPeak:'):
+                vm_peak_str = line.replace('VmPeak:', '').replace('kB', '')
+                vm_peak_kb = int(vm_peak_str)
+                break
+
+    return vm_peak_kb
