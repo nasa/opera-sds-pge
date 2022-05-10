ea42eb1bff1ba57cf68811cdb210abbee3704ddf
Ahmed Saheed <87912847+ahmedsaheed@users.noreply.github.com>
2022-05-10 18:04:15 +0100

    Fix: Consolidation of Copyright notices #91

--- .ci/scripts/build_dswx_hls.sh
+++ .ci/scripts/build_dswx_hls.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#
-# Copyright 2021-22, by the California Institute of Technology.
-# ALL RIGHTS RESERVED.
-# United States Government sponsorship acknowledged.
-# Any commercial use must be negotiated with the Office of Technology Transfer
-# at the California Institute of Technology.
-# This software may be subject to U.S. export control laws and regulations.
-# By accepting this document, the user agrees to comply with all applicable
-# U.S. export laws and regulations. User has the responsibility to obtain
-# export licenses, or other export authority as may be required, before
-# exporting such information to foreign countries or providing access to
-# foreign persons.
-#
-# Script to build the OPERA DSWx-HLS PGE Docker image
-#
-
 set -e
 
 # Parse args
