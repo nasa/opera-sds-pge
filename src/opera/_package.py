85b34dd822407030bc4798c4d2ae3ba5142927c3
Scott Collins <collinss@jpl.nasa.gov>
2022-05-17 09:47:05 -0700

    Rolled version to 1.0.0-er.4.1

--- src/opera/_package.py
+++ src/opera/_package.py
@@ -8,7 +8,7 @@ _package.py
 Package information for the OPERA PGE repository.
 """
 
-__version__ = "1.0.0-er.4.0"
+__version__ = "1.0.0-er.4.1"
 __title__ = "opera-sds-pge"
 __summary__ = "OPERA SDS Product Generation Executable (PGE) Repository"
 __uri__ = "https://github.com/nasa/opera-sds-pge"
