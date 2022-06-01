9b52ac02cd9e6440829f810c48f49ed1c35af1f1
Scott Collins <collinss@jpl.nasa.gov>
2022-06-01 11:49:01 -0700

    Updated test_metadata_utils.py to only check for osgeo.osr when skipping unit tests

--- src/opera/test/util/test_metadata_utils.py
+++ src/opera/test/util/test_metadata_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 """
-=================
+======================
 test_metadata_utils.py
-=================
+======================
 
 Unit tests for the util/metadata_utils.py module.
 
@@ -12,22 +12,17 @@ Unit tests for the util/metadata_utils.py module.
 import unittest
 from unittest import skipIf
 
-try:
-    # If mgrs, osr, or gdal are not available for import, this will raise an exception
-    from opera.util.metadata_utils import get_geographic_boundaries_from_mgrs_tile
-except:
-    pass
+from opera.util.metadata_utils import get_geographic_boundaries_from_mgrs_tile
 
-def mgrs_osr_gdal_are_available():
+
+def osr_is_available():
     """
     Helper function to check for a local installation of the Python bindings for
-    the Geospatial Data Abstraction Library (GDAL), osr and mgrs.
-    Used to skip tests that require GDAL, osr or mgrs if they are not available.
+    the Geospatial Data Abstraction Library (GDAL).
+    Used to skip tests that require GDAL if it is not available.
     """
     try:
-        from osgeo import gdal  # noqa: F401
         from osgeo import osr  # noqa: F401
-        import mgrs  # noqa: F401
         return True
     except (ImportError, ModuleNotFoundError):
         return False
@@ -36,10 +31,9 @@ def mgrs_osr_gdal_are_available():
 class MetadataUtilsTestCase(unittest.TestCase):
     """Unit test Metadata Utilities"""
 
-    @skipIf(not mgrs_osr_gdal_are_available(), reason="gdal, osr, and/or mgrs is not installed on the local instance")
+    @skipIf(not osr_is_available(), reason="osgeo.osr is not installed on the local instance")
     def test_get_geographic_boundaries_from_mgrs_tile_nominal(self):
-
-        """ Reproduce ADT results from values provided with code """
+        """Reproduce ADT results from values provided with code"""
         lat_min, lat_max, lon_min, lon_max = get_geographic_boundaries_from_mgrs_tile('15SXR')
 
         self.assertAlmostEqual(lat_min, 31.616027943130398)
@@ -47,9 +41,9 @@ class MetadataUtilsTestCase(unittest.TestCase):
         self.assertAlmostEqual(lon_min, -91.94552881416524)
         self.assertAlmostEqual(lon_max, -90.76425651871281)
 
-    @skipIf(not mgrs_osr_gdal_are_available(), reason="gdal, osr, and/or mgrs is not installed on the local instance")
+    @skipIf(not osr_is_available(), reason="osgeo.osr is not installed on the local instance")
     def test_get_geographic_boundaries_from_mgrs_tile_leading_T(self):
-
+        """Test MGRS tile code conversion when code starts with T"""
         lat_min, lat_max, lon_min, lon_max = get_geographic_boundaries_from_mgrs_tile('T15SXR')
 
         self.assertAlmostEqual(lat_min, 31.616027943130398)
@@ -57,10 +51,11 @@ class MetadataUtilsTestCase(unittest.TestCase):
         self.assertAlmostEqual(lon_min, -91.94552881416524)
         self.assertAlmostEqual(lon_max, -90.76425651871281)
 
-    @skipIf(not mgrs_osr_gdal_are_available(), reason="gdal, osr, and/or mgrs is not installed on the local instance")
+    @skipIf(not osr_is_available(), reason="osgeo.osr is not installed on the local instance")
     def test_get_geographic_boundaries_from_mgrs_tile_invalid_tile(self):
-
+        """Test MGRS tile code conversion with an invalid code"""
         self.assertRaises(RuntimeError, get_geographic_boundaries_from_mgrs_tile, 'X15SXR')
+
 
 if __name__ == "__main__":
     unittest.main()
