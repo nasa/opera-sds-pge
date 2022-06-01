541e5c6230ba692d761393a4b50d4049cfe5555d
Scott Collins <collinss@jpl.nasa.gov>
2022-06-01 11:47:16 -0700

    Added MockOsr class to metadata_utils.py
    The mock class is substituted for the real osgeo.osr module when it is missing from the current environment.
    This allows development environments to eschew the gdal installation requirement needed for production.

--- src/opera/util/metadata_utils.py
+++ src/opera/util/metadata_utils.py
@@ -1,18 +1,76 @@
 #!/usr/bin/env python3
-#
 
 """
-============
+=================
 metadata_utils.py
-============
+=================
 
 ISO metadata utilities for use with OPERA PGEs.
 
 """
 
-from osgeo import gdal
-from osgeo import osr
 import mgrs
+from mgrs.core import MGRSError
+
+
+class MockOsr:  # pragma: no cover
+    """
+    Mock class for the osgeo.osr module.
+
+    This class is defined so the opera-sds-pge project does not require the
+    Geospatial Data Abstraction Library (GDAL) as an explicit dependency for
+    developers. When PGE code is eventually run from within a Docker container,
+    osgeo.osr should always be installed and importable.
+    """
+
+    # pylint: disable=all
+    class MockSpatialReference:
+        """Mock class for the osgeo.osr module"""
+
+        def SetWellKnownGeogCS(self, name):
+            """Mock implementation for osr.SetWellKnownGeogCS"""
+            pass
+
+        def SetUTM(self, zone, north=True):
+            """Mock implementation for osr.SetUTM"""
+            self.zone = zone
+            self.hemi = "N" if north else "S"
+
+    class MockCoordinateTransformation:
+        """Mock class for the osgeo.osr.CoordinateTransformation class"""
+
+        def __init__(self, src, dest):
+            self.src = src
+            self.dest = dest
+
+        def TransformPoint(self, x, y, z):
+            """Mock implementation for CoordinateTransformation.TransformPoint"""
+            # Use mgrs to convert UTM back to a tile ID, then covert to rough
+            # lat/lon, this should be accurate enough for development testing
+            mgrs_obj = mgrs.MGRS()
+            mgrs_tile = mgrs_obj.UTMToMGRS(self.src.zone, self.src.hemi, x, y)
+            lat, lon = mgrs_obj.toLatLon(mgrs_tile)
+            return lat, lon, z
+
+    @staticmethod
+    def SpatialReference():
+        """Mock implementation for osgeo.osr.SpatialReference"""
+        return MockOsr.MockSpatialReference()
+
+    @staticmethod
+    def CoordinateTransformation(src, dest):
+        """Mock implementation for osgeo.osr.CoordinateTransformation"""
+        return MockOsr.MockCoordinateTransformation(src, dest)
+
+
+# When running a PGE within a Docker image delivered from ADT, the gdal import
+# below should work. When running in a dev environment, the import will fail
+# resulting in the MockGdal class being substituted instead.
+try:
+    from osgeo import osr
+except (ImportError, ModuleNotFoundError):  # pragma: no cover
+    osr = MockOsr                           # pragma: no cover
+
 
 def get_geographic_boundaries_from_mgrs_tile(mgrs_tile_name):
     """
@@ -22,6 +80,7 @@ def get_geographic_boundaries_from_mgrs_tile(mgrs_tile_name):
     ----------
     mgrs_tile_name : str
         MGRS tile name
+
     Returns
     -------
     lat_min : float
@@ -32,51 +91,59 @@ def get_geographic_boundaries_from_mgrs_tile(mgrs_tile_name):
         minimum longitude of bounding box
     lon_max : float
         maximum longitude of bounding box
+
+    Raises
+    ------
+    RuntimeError
+        If an invalid MGRS tile code is provided.
+
     """
- 
     # mgrs_tile_name may begin with the letter T which must be removed
     if mgrs_tile_name.startswith('T'):
         mgrs_tile_name = mgrs_tile_name[1:]
+
     mgrs_obj = mgrs.MGRS()
+
     try:
         lower_left_utm_coordinate = mgrs_obj.MGRSToUTM(mgrs_tile_name)
-    except:
+    except MGRSError as err:
         raise RuntimeError(
-            f'Failed to convert MGRS tile name "{mgrs_tile_name}" to lat/lon.'
+            f'Failed to convert MGRS tile name "{mgrs_tile_name}" to lat/lon, '
+            f'reason: {str(err)}'
         )
 
     utm_zone = lower_left_utm_coordinate[0]
     is_northern = lower_left_utm_coordinate[1] == 'N'
-    x_min = lower_left_utm_coordinate[2]
-    y_min = lower_left_utm_coordinate[3]
- 
+    x_min = lower_left_utm_coordinate[2]  # east
+    y_min = lower_left_utm_coordinate[3]  # north
+
     # create UTM spatial reference
     utm_coordinate_system = osr.SpatialReference()
     utm_coordinate_system.SetWellKnownGeogCS("WGS84")
     utm_coordinate_system.SetUTM(utm_zone, is_northern)
- 
+
     # create geographic (lat/lon) spatial reference
     wgs84_coordinate_system = osr.SpatialReference()
     wgs84_coordinate_system.SetWellKnownGeogCS("WGS84")
- 
+
     # create transformation of coordinates from UTM to geographic (lat/lon)
     transformation = osr.CoordinateTransformation(utm_coordinate_system,
                                                   wgs84_coordinate_system)
- 
+
     # compute boundaries
     elevation = 0
     lat_min = None
     lat_max = None
     lon_min = None
     lon_max = None
- 
+
     for offset_x_multiplier in range(2):
         for offset_y_multiplier in range(2):
- 
+
             x = x_min + offset_x_multiplier * 109.8 * 1000
             y = y_min + offset_y_multiplier * 109.8 * 1000
             lat, lon, z = transformation.TransformPoint(x, y, elevation)
- 
+
             if lat_min is None or lat_min > lat:
                 lat_min = lat
             if lat_max is None or lat_max < lat:
@@ -85,5 +152,5 @@ def get_geographic_boundaries_from_mgrs_tile(mgrs_tile_name):
                 lon_min = lon
             if lon_max is None or lon_max < lon:
                 lon_max = lon
- 
+
     return lat_min, lat_max, lon_min, lon_max
