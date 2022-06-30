853d177980f61734304a2d0a07b0da32adeefd80
David J Inglish <dinglish@opera-dev-pge.jpl.nasa.gov>
2022-06-30 02:43:43 +0000

    updates based on feedback

--- .ci/scripts/test_int_dswx_hls.sh
+++ .ci/scripts/test_int_dswx_hls.sh
@@ -11,21 +11,21 @@ umask 002
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
-      echo "Usage: test_int_dswx_hls.sh [-h|--help] [-p|--pge-image <image name>] [-t|--tag <tag>] [-d|--testdata <testdata .tgz file>]"
+      echo "Usage: test_int_dswx_hls.sh [-h|--help] [-t|--tag <tag>] [--testdata <testdata .tgz file>] [--runconfig <runconfig .yaml file>]"
       exit 0
-      ;;
-    -p|--pge-image)
-      PGE_IMAGE=$2
-      shift
-      shift
       ;;
     -t|--tag)
       TAG=$2
       shift
       shift
       ;;
-    -d|--testdata)
+    --testdata)
       TESTDATA=$2
+      shift
+      shift
+      ;;
+    --runconfig)
+      RUNCONFIG=$2
       shift
       shift
       ;;
@@ -41,6 +41,15 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+echo '
+================================================
+Integration Testing DSWx-HLS PGE docker image...
+================================================
+'
+
+PGE_NAME="dswx_hls"
+PGE_IMAGE="opera_pge/${PGE_NAME}"
+
 # For this version of integration test, an archive has been created which contains
 # the unmodified ADT SAS test data archive, a PGE runconfig, and PGE expected output.
 
@@ -52,13 +61,23 @@ if [ -f $TESTDATA ]; then
     echo "Extracting test data from $TESTDATA to current directory."
     tar xfz $TESTDATA
 
-    # cd into the extracted directory which must be named to match the archive file 
+    # cd into the extracted directory which must be named to match the archive file
     filename=$(basename -- "$TESTDATA")
     filename="${filename%.*}"
     cd $filename
 else
     echo "Unable to find test data file $TESTDATA"
-    exit 1 
+    exit 1
+fi
+
+if [ -f $RUNCONFIG ]; then
+    echo "Copying runconfig file $RUNCONFIG to current directory."
+    cp $RUNCONFIG .
+
+    RUNCONFIG_FILENAME=$(basename -- "$RUNCONFIG")
+else
+    echo "Unable to find runconfig file $RUNCONFIG"
+    exit 1
 fi
 
 # If any test case fails, this is set to non-zero
@@ -77,14 +96,14 @@ do
         echo "Output directory $OUTPUT_DIR already exists (and should not). Exiting."
         exit 1
     fi
-    
+
     echo "Creating output directory $OUTPUT_DIR."
     mkdir $OUTPUT_DIR
     echo "Running Docker image ${PGE_IMAGE}:${TAG} for ${DATA_DIR}"
-    docker run --rm -u conda:conda -v $(pwd):/home/conda/runconfig:ro \
+    docker run --rm -u $UID:$(id -g) -v $(pwd):/home/conda/runconfig:ro \
                      -v $(pwd)/${DATA_DIR}/input_dir:/home/conda/input_dir:ro \
                      -v $(pwd)/$OUTPUT_DIR:/home/conda/output_dir -i --tty \
-                     ${PGE_IMAGE}:${TAG} --file /home/conda/runconfig/dswx_hls.yaml
+                     ${PGE_IMAGE}:${TAG} --file /home/conda/runconfig/$RUNCONFIG_FILENAME
 
     docker_exit_status=$?
     if [ $docker_exit_status -ne 0 ]; then
@@ -104,12 +123,12 @@ do
                 then
                     echo "Not comparing log file ${output_file}"
 
-                elif [[ "${output_file##*/}" == *.tif ]]
+                elif [[ "${output_file##*/}" == *.tif* ]]
                 then
                     docker_out=$(docker run --rm -u conda:conda \
                                             -v $(pwd)/${OUTPUT_DIR}:/out:ro \
                                             -v $(pwd)/${EXPECTED_DIR}:/exp:ro \
-                                            --entrypoint python3 opera/proteus:beta \
+                                            --entrypoint python3 ${PGE_IMAGE}:${TAG} \
                                             proteus-0.1/bin/dswx_compare.py \
                                             /out/${output_file} /exp/${output_file})
                     echo "$docker_out"
@@ -121,7 +140,7 @@ do
                     fi
                 else
                     echo "Not comparing file ${output_file}"
-                fi    
+                fi
             else
                 echo "Test failed; expected file doesn't exist for output file ${output_file}."
                 OVERALL_STATUS=1
@@ -135,3 +154,4 @@ if [ $OVERALL_STATUS -ne 0 ]; then
 else
     echo "Test PASSED."
 fi
+exit $OVERALL_STATUS
