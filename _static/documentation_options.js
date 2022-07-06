f9570a1bbd177bbe8aacd52680dadcb65833e32d
Jenkins Automation Server <jenkins@opera-pge-ci.jpl.nasa.gov>
2022-07-06 20:26:56 +0000

    HTML docs update by Jenkins

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0-rc.1.0',
+    VERSION: '2.0.0-er.1.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
