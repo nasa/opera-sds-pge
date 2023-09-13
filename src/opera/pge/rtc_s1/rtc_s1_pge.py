1645a0e76f8e3cac5f07bc92fbc478dca0bd9a97
Scott Collins <collinss@jpl.nasa.gov>
2023-09-13 14:49:13 -0700

    Rolled PGE version for RTC-S1 to 2.0.0

--- src/opera/pge/rtc_s1/rtc_s1_pge.py
+++ src/opera/pge/rtc_s1/rtc_s1_pge.py
@@ -929,7 +929,7 @@ class RtcS1Executor(RtcS1PreProcessorMixin, RtcS1PostProcessorMixin, PgeExecutor
     LEVEL = "L2"
     """Processing Level for RTC-S1 Products"""
 
-    PGE_VERSION = "2.0.0-rc.2.1"
+    PGE_VERSION = "2.0.0"
     """Version of the PGE (overrides default from base_pge)"""
 
     SAS_VERSION = "1.0.0"  # Final release https://github.com/opera-adt/RTC/releases/tag/v1.0.0
