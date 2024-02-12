3414466270f9dbc728c86a9302678a3c93eaaa02
Raymond J Bambery <rjb@opera-dev-pge.jpl.nasa.gov>
2024-02-12 19:53:52 +0000

    change scripts to give dummy error reporting for SAS failure

--- src/opera/util/run_utils.py
+++ src/opera/util/run_utils.py
@@ -209,7 +209,7 @@ def time_and_execute(command_line, logger, execute_via_shell=False):
 
     if run_result.returncode:
         error_msg = (f'Command "{" ".join(command_line)}" failed with exit '
-                     f'code {run_result.returncode}')
+                f'code {run_result.returncode},last N lines fsrom log: {}')
 
         logger.critical(module_name, ErrorCode.SAS_PROGRAM_FAILED, error_msg)
 
