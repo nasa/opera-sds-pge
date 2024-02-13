c4dc98d0bdd8887671a319e210b9d0c84879dfa3
Scott Collins <72415379+collinss-jpl@users.noreply.github.com>
2024-02-13 13:18:31 -0800

    391 dswx ni stub implementation (#403)
    * add NISAR support
    * add modified scripts with NISAR
    * remove all disp_ni code, fix test sript to pass
    * fixed suggestions of Jim and Scott
    * add Dockerfile_dswx_ni and build_dswx_ni.sh
    * Removed redundant copy of test_dswx_ni_config.yaml
    * Minor additional fixes to build_dswx_ni.sh
    * Updated build_dswx_ni.sh to use the build_script_cleanup function from util.sh
    
    ---------
    
    Co-authored-by: Raymond J Bambery <rjb@opera-dev-pge.jpl.nasa.gov>
    Co-authored-by: Scott Collins <collinss@jpl.nasa.gov>

--- .ci/scripts/util/build_all_images.sh
+++ .ci/scripts/util/build_all_images.sh
@@ -33,6 +33,7 @@ ${BUILD_SCRIPTS_DIR}/cslc_s1/build_cslc_s1.sh --tag ${TAG} --workspace ${WORKSPA
 ${BUILD_SCRIPTS_DIR}/rtc_s1/build_rtc_s1.sh --tag ${TAG} --workspace ${WORKSPACE}
 ${BUILD_SCRIPTS_DIR}/dswx_s1/build_dswx_s1.sh --tag ${TAG} --workspace ${WORKSPACE}
 ${BUILD_SCRIPTS_DIR}/disp_s1/build_disp_s1.sh --tag ${TAG} --workspace ${WORKSPACE}
+${BUILD_SCRIPTS_DIR}/dswx_ni/build_dswx_ni.sh --tag ${TAG} --workspace ${WORKSPACE}
 
 echo 'Build Complete'
 
