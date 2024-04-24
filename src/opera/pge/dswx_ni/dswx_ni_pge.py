ff3e0f0c39200ba50e8ecc81dee728c6b53cd572
Scott Collins <collinss@jpl.nasa.gov>
2024-04-24 11:17:03 -0700

    Updated dswx_s1_pge.py and dswx_ni_pge.py so they can share the implementation of the DSWx-S1 preprocessor

--- src/opera/pge/dswx_ni/dswx_ni_pge.py
+++ src/opera/pge/dswx_ni/dswx_ni_pge.py
@@ -10,7 +10,6 @@ from NISAR (NI) PGE.
 
 from opera.pge.base.base_pge import PgeExecutor
 from opera.pge.base.base_pge import PostProcessorMixin
-from opera.pge.base.base_pge import PreProcessorMixin
 from opera.pge.dswx_s1.dswx_s1_pge import DSWxS1PreProcessorMixin
 
 class DSWxNIPreProcessorMixin(DSWxS1PreProcessorMixin):
@@ -18,20 +17,22 @@ class DSWxNIPreProcessorMixin(DSWxS1PreProcessorMixin):
     Mixin class responsible for handling all pre-processing steps for the DSWX-NI
     PGE. The pre-processing phase is defined as all steps necessary prior
     to SAS execution.
-    In addition to the base functionality inherited from PreProcessorMixin, this
-    mixin adds an input validation step to ensure that input(s) defined by the
-    RunConfig exist and are valid.
 
-     """
+    This particular pre-processor inherits its functionality from the DSWx-S1
+    pre-processor class, as both PGE's share a similar interface.
+
+    """
 
     _pre_mixin_name = "DSWxNIPreProcessorMixin"
+    _valid_input_extensions = (".h5",)
 
     def run_preprocessor(self, **kwargs):
         """
         Executes the pre-processing steps for DSWx-NI PGE initialization.
-        The DswxS1PreProcessorMixin version of this class performs all actions
-        of the base PreProcessorMixin class, and adds an input validation step for
-        the inputs defined within the RunConfig (TODO).
+        The DSWxNIPreProcessorMixin version of this class performs all actions
+        of the DSWxS1PreProcessorMixin class. Parameterization of the validation
+        functions is handled via specialized class attributes (i.e. _valid_input_extensions)
+
         Parameters
         ----------
         **kwargs: dict
@@ -39,66 +40,6 @@ class DSWxNIPreProcessorMixin(DSWxS1PreProcessorMixin):
         """
         super().run_preprocessor(**kwargs)
 
-    def _validate_dynamic_ancillary_inputs(self):
-        """
-        Evaluates the list of dynamic ancillary inputs from the RunConfig to
-        ensure they exist and have an expected file extension.
-
-        """
-        dynamic_ancillary_file_group_dict = \
-            self.runconfig.sas_config['runconfig']['groups']['dynamic_ancillary_file_group']
-
-        for key, value in dynamic_ancillary_file_group_dict.items():
-            if key in ('dem_file', 'reference_water_file', 'worldcover_file', 'hand_file'):
-                input_validation.check_input(
-                    value, self.logger, self.name, valid_extensions=('.tif', '.tiff', '.vrt')
-                )
-            elif key in ('shoreline_shapefile',):
-                if value is not None:
-                    input_validation.check_input(
-                        value, self.logger, self.name, valid_extensions=('.shp',))
-                    # Only the .shp file is configured in the runconfig, but we
-                    # need to ensure the other required files are co-located with it
-                    for extension in ('.dbf', '.prj', '.shx'):
-                        additional_shapefile = splitext(value)[0] + extension
-
-                        if not exists(abspath(additional_shapefile)):
-                            error_msg = f"Additional shapefile {additional_shapefile} could not be located"
-
-                            self.logger.critical(self.name, ErrorCode.INVALID_INPUT, error_msg)
-                else:
-                    msg = "No shoreline_shapefile specified in runconfig file."
-                    self.logger.info(self.name, ErrorCode.INPUT_NOT_FOUND, msg)
-
-            elif key in ('dem_file_description', 'worldcover_file_description',
-                         'reference_water_file_description', 'hand_file_description',
-                         'shoreline_shapefile_description'):
-                # these fields are included in the SAS input paths, but are not
-                # actually file paths, so skip them
-                continue
-            elif key in ('algorithm_parameters',):
-                input_validation.check_input(
-                    value, self.logger, self.name, valid_extensions=('.yaml', )
-                )
-
-    def _validate_static_ancillary_inputs(self):
-        """
-        Evaluates the list of static ancillary inputs from the RunConfig to
-        ensure they exist and have an expected file extension.
-
-        """
-        static_ancillary_file_group_dict = \
-            self.runconfig.sas_config['runconfig']['groups']['static_ancillary_file_group']
-
-        for key, value in static_ancillary_file_group_dict.items():
-            if key in ('mgrs_database_file', 'mgrs_collection_database_file'):
-                input_validation.check_input(
-                    value, self.logger, self.name, valid_extensions=('.sqlite', '.sqlite3')
-                )
-            elif key in ('static_ancillary_inputs_flag', ):
-                # these fields are included in the SAS input paths, but are not
-                # actually file paths, so skip them
-                continue
 
 class DSWxNIPostProcessorMixin(PostProcessorMixin):
     """
