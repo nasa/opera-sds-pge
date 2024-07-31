9df10a4bf31d4595a99e0de4c779ab39558b928b
rileykk <rileykk@jpl.nasa.gov>
2024-07-31 14:37:40 -0700

    CI work - update

--- .ci/scripts/util/compare_secrets_baselines.sh
+++ .ci/scripts/util/compare_secrets_baselines.sh
@@ -6,6 +6,10 @@ compare_secrets() {
 
   diff /data/tmp/.secrets.1 /data/tmp/.secrets.2 > .secrets.diff
 
+  rm -f /data/tmp/.secrets.1 /data/tmp/.secrets.2
+
   grep -q '>' < .secrets.diff
+
+  # This causes a syntax issue in the pipeline...
   # diff <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort) <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort) | grep -q '>' ;
 }
