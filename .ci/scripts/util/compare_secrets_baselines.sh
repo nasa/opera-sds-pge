19cdce9b70db6456d034ddbb0f67265b1f536a51
rileykk <rileykk@jpl.nasa.gov>
2024-07-31 14:31:28 -0700

    CI work - update

--- .ci/scripts/util/compare_secrets_baselines.sh
+++ .ci/scripts/util/compare_secrets_baselines.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 compare_secrets() {
-#  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort > /data/tmp/.secrets.1
-#  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort > /data/tmp/.secrets.1
+  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort > /data/tmp/.secrets.1
+  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort > /data/tmp/.secrets.2
 
+  diff /data/tmp/.secrets.1 /data/tmp/.secrets.2 > .secrets.diff
 
-  diff <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort) <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort) | grep -q '>' ;
+  grep -q '>' < .secrets.diff
+  # diff <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort) <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort) | grep -q '>' ;
 }
