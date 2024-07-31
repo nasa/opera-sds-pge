1f6bff879440a00c19e61f96b9048d3433d45b15
rileykk <rileykk@jpl.nasa.gov>
2024-07-31 14:28:17 -0700

    CI work - update

--- .ci/scripts/util/compare_secrets_baselines.sh
+++ .ci/scripts/util/compare_secrets_baselines.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 compare_secrets() {
-  diff <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort) <(jq -r '.results | keys[] as $key | "\\($key),\\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort) | grep -q '>' ;
+#  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort > /data/tmp/.secrets.1
+#  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort > /data/tmp/.secrets.1
+
+
+  diff <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${1}" | sort) <(jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "${2}" | sort) | grep -q '>' ;
 }
