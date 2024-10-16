26c80107d27f4c5966fa351f11181f0f1b5a8477
Scott Collins <collinss@jpl.nasa.gov>
2024-10-16 16:57:20 -0500

    Rolled DISP-S1 SAS version to 0.4.6 in disp_s1_pge.py

--- src/opera/pge/disp_s1/disp_s1_pge.py
+++ src/opera/pge/disp_s1/disp_s1_pge.py
@@ -836,7 +836,7 @@ class DispS1Executor(DispS1PreProcessorMixin, DispS1PostProcessorMixin, PgeExecu
     PGE_VERSION = "3.0.0-rc.4.1"
     """Version of the PGE (overrides default from base_pge)"""
 
-    SAS_VERSION = "0.4.4"  # CalVal release https://github.com/opera-adt/disp-s1/releases/tag/v0.4.4
+    SAS_VERSION = "0.4.6"  # CalVal release https://github.com/opera-adt/disp-s1/releases/tag/v0.4.6
 
     def __init__(self, pge_name, runconfig_path, **kwargs):
         super().__init__(pge_name, runconfig_path, **kwargs)
