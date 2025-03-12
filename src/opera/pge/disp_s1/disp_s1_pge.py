5596ab2b957827c1784f3054f3b59dc0d633167e
Scott Collins <collinss@jpl.nasa.gov>
2025-03-12 09:25:03 -0700

    Rolled DISP-S1 PGE version numbers to 3.0.4

--- src/opera/pge/disp_s1/disp_s1_pge.py
+++ src/opera/pge/disp_s1/disp_s1_pge.py
@@ -849,7 +849,7 @@ class DispS1Executor(DispS1PreProcessorMixin, DispS1PostProcessorMixin, PgeExecu
     LEVEL = "L3"
     """Processing Level for DISP-S1 Products"""
 
-    PGE_VERSION = "3.0.3"
+    PGE_VERSION = "3.0.4"
     """Version of the PGE (overrides default from base_pge)"""
 
     SAS_VERSION = "0.5.5"  # Final release https://github.com/opera-adt/disp-s1/releases/tag/v0.5.5
