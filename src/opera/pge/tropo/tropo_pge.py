5f5bf4dc433995377141d3b7a4d57739d66e65e5
kevinmarlis <kevinmarlis@gmail.com>
2025-03-12 13:38:32 -0700

    Overriding _checksum_output_products in order to use custom file filter

--- src/opera/pge/tropo/tropo_pge.py
+++ src/opera/pge/tropo/tropo_pge.py
@@ -7,9 +7,12 @@ tropo_pge.py
 Module defining the implementation for the TROPO PGE.
 """
 
+from os.path import basename, splitext
+
 from opera.pge.base.base_pge import PgeExecutor
 from opera.pge.base.base_pge import PostProcessorMixin
 from opera.pge.base.base_pge import PreProcessorMixin
+from opera.util.run_utils import get_checksum
 
 
 class TROPOPreProcessorMixin(PreProcessorMixin):
@@ -51,6 +54,40 @@ class TROPOPostProcessorMixin(PostProcessorMixin):
 
     _post_mixin_name = "TROPOPostProcessorMixin"
     _cached_core_filename = None
+    
+    def _checksum_output_products(self):
+        """
+        Generates a dictionary mapping output product file names to the
+        corresponding MD5 checksum digest of the file's contents.
+
+        The products to generate checksums for are determined by scanning
+        the output product location specified by the RunConfig. Any files
+        within the directory that have the expected file extensions for output
+        products are then picked up for checksum generation.
+
+        Returns
+        -------
+        checksums : dict
+            Mapping of output product file names to MD5 checksums of said
+            products.
+
+        """
+        output_products = self.runconfig.get_output_product_filenames()
+
+        # Filter out any files that do not end with the expected extensions
+        expected_extensions = ('.nc', '.png')
+        filtered_output_products = filter(
+            lambda product: splitext(product)[-1] in expected_extensions,
+            output_products
+        )
+
+        # Generate checksums on the filtered product list
+        checksums = {
+            basename(output_product): get_checksum(output_product)
+            for output_product in filtered_output_products
+        }
+
+        return checksums
 
     def run_postprocessor(self, **kwargs):
         """
@@ -64,7 +101,11 @@ class TROPOPostProcessorMixin(PostProcessorMixin):
         **kwargs: dict
             Any keyword arguments needed by the post-processor
         """
-        super().run_postprocessor(**kwargs)
+        self._validate_output()
+        self._validate_output_product_filenames()
+        self._run_sas_qa_executable()
+        self._stage_output_files()
+        # super().run_postprocessor(**kwargs)
 
 
 class TROPOExecutor(TROPOPreProcessorMixin, TROPOPostProcessorMixin, PgeExecutor):
