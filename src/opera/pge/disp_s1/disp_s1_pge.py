aff9ffaef7f2524d27a76c364ea5ff01641c8b79
rileykk <rileykk@jpl.nasa.gov>
2025-03-28 08:39:07 -0700

    Address some additional feedback items

--- src/opera/pge/disp_s1/disp_s1_pge.py
+++ src/opera/pge/disp_s1/disp_s1_pge.py
@@ -1038,13 +1038,14 @@ class DispS1StaticPostProcessorMixin(DispS1PostProcessorMixin):
             The file name to assign to GeoTIFF product(s) created by this PGE.
         """
 
-        # I think there should be a better way to do this. The SAS outputs just the band names
-        # (ie, dem_warped_utm.tif) But I need 'dist-s1-static' in the mock filenames to match
-        # the mock GDAL dataset, so I need this code to handle both filenames. I can't split on
-        # '_' and mock the final OPERA naming convention since the layers have a varying number
-        # of underscores, so I settled to mocking "dist-s1-static-<layer>" and splitting by "-"
-        # to get the last field which is the band name. The SAS outputs don't contain any "-"s
-        # so this split has no effect.
+        # TODO:
+        #  I think there should be a better way to do this. The SAS outputs just the band names
+        #  (ie, dem_warped_utm.tif) But I need 'dist-s1-static' in the mock filenames to match
+        #  the mock GDAL dataset, so I need this code to handle both filenames. I can't split on
+        #  '_' and mock the final OPERA naming convention since the layers have a varying number
+        #  of underscores, so I settled to mocking "dist-s1-static-<layer>" and splitting by "-"
+        #  to get the last field which is the band name. The SAS outputs don't contain any "-"s
+        #  so this split has no effect.
 
         band_name = splitext(basename(inter_filename))[0].split('-')[-1]
 
@@ -1120,7 +1121,8 @@ class DispS1StaticPostProcessorMixin(DispS1PostProcessorMixin):
             'ISO_OPERA_ProducerGranuleId': self._core_filename(inter_filename),
             'MetadataProviderAction': "creation",
             'GranuleFilename': self._core_filename(inter_filename),
-            'ISO_OPERA_ProjectKeywords': ['OPERA', 'JPL', 'DISP', 'Displacement', 'Surface', 'Land', 'Global'],
+            'ISO_OPERA_ProjectKeywords': ['OPERA', 'JPL', 'DISP', 'Displacement',
+                                          'Surface', 'Land', 'Global', 'STATIC'],
             'ISO_OPERA_PlatformKeywords': ['S1'],
             'ISO_OPERA_InstrumentKeywords': ['Sentinel 1 A/B']
         }
