4c724a515c32d3f090bdcab3d3eab4e1046c59d3
rileykk <rileykk@jpl.nasa.gov>
2025-03-31 11:17:39 -0700

    Integrate DISP-S1 SAS v 0.5.7

--- src/opera/pge/disp_s1/disp_s1_pge.py
+++ src/opera/pge/disp_s1/disp_s1_pge.py
@@ -894,7 +894,7 @@ class DispS1Executor(DispS1PreProcessorMixin, DispS1PostProcessorMixin, PgeExecu
     PGE_VERSION = "3.0.4"
     """Version of the PGE (overrides default from base_pge)"""
 
-    SAS_VERSION = "0.5.6"  # Final release https://github.com/opera-adt/disp-s1/releases/tag/v0.5.6
+    SAS_VERSION = "0.5.7"  # Final release https://github.com/opera-adt/disp-s1/releases/tag/v0.5.7
 
     def __init__(self, pge_name, runconfig_path, **kwargs):
         super().__init__(pge_name, runconfig_path, **kwargs)
