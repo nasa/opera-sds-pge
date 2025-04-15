b0e212d41c01f4b8dc43f3bc466fa3b7eda88fe7
Kevin <kevinmarlis@gmail.com>
2025-04-15 15:43:48 -0700

    TROPO CalVal v0.2 Integration (#642)
    * Updated image tag to use calval_0.2
    
    * Updated expected input and output data archives along with runconfig for CalVal 0.2
    
    * Updated SAS version to 0.2
    
    * Updates to reflect ADT changes to SAS config along with version bumps and updated input filepaths
    
    * Fix for InputFilePaths in PGE group
    
    * Updates to max_memory field in worker_settings to account for GB specification
    
    * Added output_heights explicitly as an empty list no longer results in using default values

--- .ci/scripts/tropo/test_int_tropo.sh
+++ .ci/scripts/tropo/test_int_tropo.sh
@@ -28,9 +28,9 @@ SAMPLE_TIME=1
 # RUNCONFIG should be the name of the runconfig file  in <repo_root>/.ci/scripts/tropo/
 [ -z "${WORKSPACE}" ] && WORKSPACE=$(realpath "$(dirname "$(realpath "$0")")"/../../..)
 [ -z "${PGE_TAG}" ] && PGE_TAG="${USER}-dev"
-[ -z "${INPUT_DATA}" ] && INPUT_DATA="tropo_interface_0.1_expected_input.zip "
-[ -z "${EXPECTED_DATA}" ] && EXPECTED_DATA="tropo_interface_0.1_expected_output.zip "
-[ -z "${RUNCONFIG}" ] && RUNCONFIG="opera_pge_tropo_0.1_beta_runconfig.yaml"
+[ -z "${INPUT_DATA}" ] && INPUT_DATA="tropo_calval_0.2_expected_input.zip "
+[ -z "${EXPECTED_DATA}" ] && EXPECTED_DATA="tropo_calval_0.2_expected_output.zip "
+[ -z "${RUNCONFIG}" ] && RUNCONFIG="opera_pge_tropo_0.2_calval_runconfig.yaml"
 [ -z "${TMP_ROOT}" ] && TMP_ROOT="$DEFAULT_TMP_ROOT"
 
 # Create the test output directory in the work space
