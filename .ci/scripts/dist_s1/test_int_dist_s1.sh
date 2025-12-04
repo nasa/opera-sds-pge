e858afc9140d15e665caa1356291641af30e8f38
rileykk <rileykk@jpl.nasa.gov>
2025-12-04 15:05:06 -0800

    update int script versions

--- .ci/scripts/dist_s1/test_int_dist_s1.sh
+++ .ci/scripts/dist_s1/test_int_dist_s1.sh
@@ -28,9 +28,9 @@ SAMPLE_TIME=1
 # RUNCONFIG should be the name of the runconfig file  in <repo_root>/.ci/scripts/dist-s1/
 [ -z "${WORKSPACE}" ] && WORKSPACE=$(realpath "$(dirname "$(realpath "$0")")"/../../..)
 [ -z "${PGE_TAG}" ] && PGE_TAG="${USER}-dev"
-[ -z "${INPUT_DATA}" ] && INPUT_DATA="dist_s1_calval_2.0.7_expected_input.zip "
-[ -z "${EXPECTED_DATA}" ] && EXPECTED_DATA="dist_s1_calval_2.0.7_expected_output.zip "
-[ -z "${RUNCONFIG}" ] && RUNCONFIG="opera_pge_dist_s1_r4.3_calval_runconfig.yaml"
+[ -z "${INPUT_DATA}" ] && INPUT_DATA="dist_s1_calval_2.0.8_expected_input.zip "
+[ -z "${EXPECTED_DATA}" ] && EXPECTED_DATA="dist_s1_calval_2.0.8_expected_output.zip "
+[ -z "${RUNCONFIG}" ] && RUNCONFIG="opera_pge_dist_s1_r4.4_calval_runconfig.yaml"
 [ -z "${TMP_ROOT}" ] && TMP_ROOT="$DEFAULT_TMP_ROOT"
 
 # Create the test output directory in the work space
@@ -85,7 +85,7 @@ echo "Creating scratch directory $scratch_dir."
 mkdir -p --mode=777 "$scratch_dir"
 
 # Copy the Algorithm Parameters RunConfigs
-algo_runconfig="opera_pge_dist_s1_r4.3_calval_algorithm_parameters.yaml"
+algo_runconfig="opera_pge_dist_s1_r4.4_calval_algorithm_parameters.yaml"
 local_algo_runconfig="${SCRIPT_DIR}/${algo_runconfig}"
 echo "Copying runconfig file $local_algo_runconfig to $runconfig_dir"
 cp ${local_algo_runconfig} ${runconfig_dir}
