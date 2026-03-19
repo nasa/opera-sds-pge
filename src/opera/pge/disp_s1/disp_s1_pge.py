beadddca3b432560e3cdb35dda7349a3a8d26f9c
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-03-19 11:54:33 -0700

    bump version numbers

--- src/opera/pge/disp_s1/disp_s1_pge.py
+++ src/opera/pge/disp_s1/disp_s1_pge.py
@@ -901,7 +901,7 @@ class DispS1Executor(DispS1PreProcessorMixin, DispS1PostProcessorMixin, PgeExecu
     PGE_VERSION = "3.0.9"
     """Version of the PGE (overrides default from base_pge)"""
 
-    SAS_VERSION = "0.5.12"  # Final release https://github.com/opera-adt/disp-s1/releases/tag/v0.5.12
+    SAS_VERSION = "0.5.13"  # Final release https://github.com/opera-adt/disp-s1/releases/tag/v0.5.13
 
     def __init__(self, pge_name, runconfig_path, **kwargs):
         super().__init__(pge_name, runconfig_path, **kwargs)
