fc0d54b36b0dd68339e5cb06bcb5d7ade664c0b1
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-07-23 18:21:36 -0700

    fix condition

--- .ci/scripts/cslc_s1/compare_cslc_s1_static_products.sh
+++ .ci/scripts/cslc_s1/compare_cslc_s1_static_products.sh
@@ -64,11 +64,11 @@ for burst_id in "${burst_ids[@]}"; do
         echo "Failure: All CSLC metadata checks DID NOT PASS"
         static_layers_compare_result="FAIL"
         overall_status=2
-    elif [[ "$compare_out" != *"ERROR reference static_layers product not found"* ]]; then
+    elif [[ "$compare_out" == *"ERROR reference static_layers product not found"* ]]; then
         echo "Failure: Product not found"
         static_layers_compare_result="FAIL"
         overall_status=2
-    elif [[ "$compare_out" != *"ERROR reference CSLC metadata not found"* ]]; then
+    elif [[ "$compare_out" == *"ERROR reference CSLC metadata not found"* ]]; then
         echo "Failure: Product not found"
         static_layers_compare_result="FAIL"
         overall_status=2
